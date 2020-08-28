fac4eb4d7761b4d18c834748712f45e4519b8f65
Cary Dean <Cary@Cary-Deans-MacBook-Pro.local>
2020-08-27 21:05:16 -0400

    Added Spin Wheel

---
+++ spinwheel/precache-manifest.563dea8a30023cf910bd67bc65f6298b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a7e037ecd57bc21064e5651ea602e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29c1a25c10367bcf4944",
+    "url": "/static/css/main.9fb0e00d.chunk.css"
+  },
+  {
+    "revision": "5badaabfd05ac9161808",
+    "url": "/static/js/2.e8485617.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e8485617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29c1a25c10367bcf4944",
+    "url": "/static/js/main.565c18b3.chunk.js"
+  },
+  {
+    "revision": "e0ca405b85c4de0031ce",
+    "url": "/static/js/runtime-main.51f621c8.js"
+  }
+]);
